78d60345c597861f35f5399be36f0917fc5779bc
b2evandro <55710708+b2evandro@users.noreply.github.com>
2020-08-19 20:22:34 -0300

    inserindo js
    Sem integração para gifs e outras extenções

--- scripts.js
+++ scripts.js
@@ -2,34 +2,70 @@
 const sectionCards = document.querySelector("section.cards");
 
 const card = document.querySelector("div.card");
-/* 
-const videos = [
-  {
-    title: "Esse aqui  Animado com JS puro e CSS Animation | Mayk Brito",
-    duration: "57 min",
-    thumb: "img/jpg/D020.jpg",
-    video_id: "GykTLqODQuU"
-  },
-  {
-    title: "Desvendando o CSS Grid na prática | Mayk Brito",
-    duration: "36 min",
-    thumb: "img/jpg/Gigante1024.jpg",
-    video_id: "HN1UjzRSdBk"
-  },
-  {
-    title: "Array: Higher Order Functions | Mayk Brito",
-    duration: "54 min",
-    thumb: "img/jpg/D019.jpg",
-    video_id: "rAzHvYnQ8DY"
-  }
-]; */
 
-videos.map(video => {
+const jpgs = [
+  {
+    jpg_id: "D011.jpg",
+    thumb: "img/jpg/D011.jpg"
+  },
+  {
+    jpg_id: "D020.jpg",
+    thumb: "img/jpg/D020.jpg"
+  },
+  {
+    jpg_id: "Gigante1024.jpg",
+    thumb: "img/jpg/Gigante1024.jpg"
+  },
+  {
+    jpg_id:"D019.jpg",
+    thumb: "img/jpg/D019.jpg"
+  },
+  {
+    jpg_id:"foto4.jpg",
+    thumb: "img/jpg/foto4.jpg"
+  },
+  {
+    jpg_id:"NewZeland_1024.jpg",
+    thumb: "img/jpg/NewZeland_1024.jpg"
+  }
+];
+
+
+jpgs.map(jpg => {
   const cardClone = card.cloneNode(true);
-  cardClone.setAttribute("id", video.video_id);
-  cardClone.querySelector("img").src = video.thumb;
-  cardClone.querySelector(".title").innerHTML = video.title;
-  cardClone.querySelector(".info > p.text--medium").innerHTML =
-    video.duration;
+  cardClone.querySelector("img").src = jpg.thumb;
+  cardClone.setAttribute("id", jpg.jpg_id);
   sectionCards.appendChild(cardClone);
 });
+
+  
+
+card.remove();
+
+// Modal actions
+const modalOverlay = document.querySelector(".modal-overlay");
+const modal = document.querySelector(".modal");
+const cards = [...document.querySelectorAll(".cards .card")];
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    modal.querySelector(
+      ".Im1"
+    ).src = `img/jpg/${card.getAttribute("id")}`;
+    modalOverlay.classList.add("active");
+    modal.classList.add("active");
+    document.querySelector("body").style.overflow = "hidden";
+  });
+});
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+  modalOverlay.classList.remove("active");
+  modal.classList.remove("active");
+  modal.querySelector(".Im1").src = ``;
+  document.querySelector("body").style.overflow = "initial";
+});
+
+
+
+
+// MENU RESPONSIVO
