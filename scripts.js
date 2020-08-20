d598dcaf971f778d14d6694782cb6346961078fc
B2evandro <evandro.sl@aluno.ifsc.edu.br>
2020-08-20 17:41:10 -0300

    adicionando esc
    alterando conteudo e adicionando esc

--- scripts.js
+++ scripts.js
@@ -1,71 +1,75 @@
 // Make Cards
-const sectionCards = document.querySelector("section.cards");
-
-const card = document.querySelector("div.card");
-
+const sectionCards_jpg = document.querySelector("section.cards_jpg");
+const card_jpg = document.querySelector("div.card_jpg");
 const jpgs = [
   {
-    jpg_id: "D011.jpg",
+    jpg_id: "img/jpg/D011.jpg",
     thumb: "img/jpg/D011.jpg"
   },
   {
-    jpg_id: "D020.jpg",
+    jpg_id: "img/jpg/D020.jpg",
     thumb: "img/jpg/D020.jpg"
   },
   {
-    jpg_id: "Gigante1024.jpg",
+    jpg_id: "img/jpg/Gigante1024.jpg",
     thumb: "img/jpg/Gigante1024.jpg"
   },
   {
-    jpg_id:"D019.jpg",
+    jpg_id: "img/jpg/D019.jpg",
     thumb: "img/jpg/D019.jpg"
   },
   {
-    jpg_id:"foto4.jpg",
+    jpg_id: "img/jpg/foto4.jpg",
     thumb: "img/jpg/foto4.jpg"
   },
   {
-    jpg_id:"NewZeland_1024.jpg",
+    jpg_id: "img/jpg/NewZeland_1024.jpg",
     thumb: "img/jpg/NewZeland_1024.jpg"
   }
 ];
-
-
 jpgs.map(jpg => {
-  const cardClone = card.cloneNode(true);
-  cardClone.querySelector("img").src = jpg.thumb;
-  cardClone.setAttribute("id", jpg.jpg_id);
-  sectionCards.appendChild(cardClone);
+  const card_jpgClone = card_jpg.cloneNode(true);
+  card_jpgClone.querySelector("img").src = jpg.thumb;
+  card_jpgClone.setAttribute("id", jpg.jpg_id);
+  sectionCards_jpg.appendChild(card_jpgClone);
 });
-
-  
-
-card.remove();
-
+card_jpg.remove();
 // Modal actions
 const modalOverlay = document.querySelector(".modal-overlay");
 const modal = document.querySelector(".modal");
-const cards = [...document.querySelectorAll(".cards .card")];
+const cards_jpg = [...document.querySelectorAll(".cards_jpg .card_jpg")];
 
-cards.forEach(card => {
-  card.addEventListener("click", () => {
+cards_jpg.forEach(card_jpg => {
+  card_jpg.addEventListener("click", () => {
     modal.querySelector(
       ".Im1"
-    ).src = `img/jpg/${card.getAttribute("id")}`;
+    ).src = `${card_jpg.getAttribute("id")}`;
     modalOverlay.classList.add("active");
     modal.classList.add("active");
     document.querySelector("body").style.overflow = "hidden";
   });
 });
 
-document.querySelector(".close-modal").addEventListener("click", () => {
+function closemodal() {
   modalOverlay.classList.remove("active");
   modal.classList.remove("active");
   modal.querySelector(".Im1").src = ``;
   document.querySelector("body").style.overflow = "initial";
+}
+
+document.querySelector(".close-modal").addEventListener("click", () => {
+  closemodal();
 });
-
-
-
-
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+    isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  } else {
+    isEscape = (evt.keyCode === 27);
+  }
+  if (isEscape) {
+    closemodal();
+  }
+};
 // MENU RESPONSIVO
