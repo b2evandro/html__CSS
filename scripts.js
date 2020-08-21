4110e3ae35574d0e6e05b8d1f7e2288dd9576475
B2evandro <evandro.sl@aluno.ifsc.edu.br>
2020-08-21 15:07:05 -0300

    js adcionando gifs

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // Make Cards
-const sectionCards_jpg = document.querySelector("section.cards_jpg");
+const sectionCards_jpg = document.querySelector("section.cards_jpg ");
 const card_jpg = document.querySelector("div.card_jpg");
 const jpgs = [
   {
@@ -27,6 +27,7 @@ const jpgs = [
     thumb: "img/jpg/NewZeland_1024.jpg"
   }
 ];
+
 jpgs.map(jpg => {
   const card_jpgClone = card_jpg.cloneNode(true);
   card_jpgClone.querySelector("img").src = jpg.thumb;
@@ -72,4 +73,50 @@ document.onkeydown = function (evt) {
     closemodal();
   }
 };
+
+const sectionCards_gif = document.querySelector("section.cards_gif");
+const card_gif = document.querySelector("div.card_gif");
+const gifs = [
+  {
+    gif_id: "img/gif/G001.gif",
+    thumb: "img/gif/G001.gif"
+  },
+  {
+    gif_id: "img/gif/G198.gif",
+    thumb: "img/gif/G198.gif"
+  },
+  {
+    gif_id: "img/gif/G012.gif",
+    thumb: "img/gif/G012.gif"
+  }
+];
+
+gifs.map(gif => {
+  const card_gifClone = card_gif.cloneNode(true);
+  card_gifClone.querySelector("img").src = gif.thumb;
+  card_gifClone.setAttribute("id", gif.gif_id);
+  sectionCards_gif.appendChild(card_gifClone);
+});
+card_gif.remove()
+
+// Modal actions
+const cards_gif = [...document.querySelectorAll(".cards_gif .card_gif")];
+
+cards_gif.map(card_gif => {
+  card_gif.addEventListener("click", () => {
+    modal.querySelector(
+      ".Im1"
+    ).src = `${card_gif.getAttribute("id")}`;
+    modalOverlay.classList.add("active");
+    modal.classList.add("active");
+    document.querySelector("body").style.overflow = "hidden";
+  });
+});
+
+function closemodal() {
+  modalOverlay.classList.remove("active");
+  modal.classList.remove("active");
+  modal.querySelector(".Im1").src = ``;
+  document.querySelector("body").style.overflow = "initial";
+}
 // MENU RESPONSIVO
