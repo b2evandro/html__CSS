95ea8e552e201319225c89226a2c0349bfb4770f
b2evandro <55710708+b2evandro@users.noreply.github.com>
2020-10-08 21:23:24 -0300

    ancora

--- menu.js
+++ menu.js
@@ -1,35 +1,13 @@
-
-let show = true;
+  
+let show = true; 
 
 const menuSection = document.querySelector(".menu-section")
 const menuToggle = menuSection.querySelector(".menu-toggle")
-const opcoes = document.querySelector(".opcoes");
-
-
-function show_option() {
-    console.log(show)
-    document.body.style.overflow = "hidden" ? "initial" : "hidden"
-    menuSection.classList.toggle("on", show)
-    show = !show;
-
-    $('.nav a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        var id = $(this).attr('href'),
-            targetOffset = $(id).offset().top;
-
-        $('html, body').animate({
-            scrollTop: targetOffset - 100
-        }, 500);
-    });
-
-    console.log(show)
-}
-
-opcoes.addEventListener("click", () => {
-    show_option();
-
-})
 
 menuToggle.addEventListener("click", () => {
-    show_option();
+
+    document.body.style.overflow = show ? "hidden" : "initial"
+
+    menuSection.classList.toggle("on", show)
+    show = !show;
 })
